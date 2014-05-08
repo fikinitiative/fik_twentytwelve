c9aa56ae8fef144c19db70aee8ba402594653a51
minayaserrano <minayaserrano@gmail.com>
2014-05-08 19:41:20 +0200

    Fix custom header page admin

--- functions.php
+++ functions.php
@@ -921,3 +921,46 @@ if (!function_exists('twentytwelve_comment')) :
     add_filter('excerpt_more', 'new_excerpt_more');
 
     add_action('wp_footer', 'fik_legal_terms_link', 100);
+
+    /**
+     * Return the Google font stylesheet URL if available.
+     *
+     * The use of Open Sans by default is localized. For languages that use
+     * characters not supported by the font, the font can be disabled.
+     *
+     * @since Twenty Twelve 1.2
+     *
+     * @return string Font stylesheet or empty string if disabled.
+     */
+    function twentytwelve_get_font_url() {
+        $font_url = '';
+
+        /* translators: If there are characters in your language that are not supported
+         * by Open Sans, translate this to 'off'. Do not translate into your own language.
+         */
+        if ( 'off' !== _x( 'on', 'Open Sans font: on or off', 'twentytwelve' ) ) {
+            $subsets = 'latin,latin-ext';
+
+            /* translators: To add an additional Open Sans character subset specific to your language,
+             * translate this to 'greek', 'cyrillic' or 'vietnamese'. Do not translate into your own language.
+             */
+            $subset = _x( 'no-subset', 'Open Sans font: add new subset (greek, cyrillic, vietnamese)', 'twentytwelve' );
+
+            if ( 'cyrillic' == $subset )
+                $subsets .= ',cyrillic,cyrillic-ext';
+            elseif ( 'greek' == $subset )
+                $subsets .= ',greek,greek-ext';
+            elseif ( 'vietnamese' == $subset )
+                $subsets .= ',vietnamese';
+
+            $protocol = is_ssl() ? 'https' : 'http';
+            $query_args = array(
+                'family' => 'Open+Sans:400italic,700italic,400,700',
+                'subset' => $subsets,
+            );
+            $font_url = add_query_arg( $query_args, "$protocol://fonts.googleapis.com/css" );
+        }
+
+        return $font_url;
+    }
+
